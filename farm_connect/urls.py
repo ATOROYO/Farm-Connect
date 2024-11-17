5e38aaa6a801586d62e74178b1cebe7bf72e68ad
ATOROYO <davidatoroyosika@gmail.com>
2024-11-17 17:14:25 +0300

    Added farmers's path to the urls.path

--- farm_connect/urls.py
+++ farm_connect/urls.py
@@ -3,4 +3,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('about/', TemplateView.as_view(template_name="about.html"), name='about'),
+    path('farmer/dashboard/', TemplateView.as_view(template_name="farmer_dashboard.html"), name='farmer_dashboard'),
 ]
