52284238f301d6b4520f141b3c1dede11b379456
ATOROYO <davidatoroyosika@gmail.com>
2024-11-19 15:40:15 +0300

    Added the url path for the supplier page

--- farm_connect/urls.py
+++ farm_connect/urls.py
@@ -4,4 +4,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('about/', TemplateView.as_view(template_name="about.html"), name='about'),
     path('farmer/dashboard/', TemplateView.as_view(template_name="farmer_dashboard.html"), name='farmer_dashboard'),
+    path('supplier/dashboard/', TemplateView.as_view(template_name="supplier_dashboard.html"), name='supplier_dashboard')
 ]
