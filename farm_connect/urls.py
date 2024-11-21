8b238d85b2daac60dd1eb56be1ba8c72f36b67dc
ATOROYO <davidatoroyosika@gmail.com>
2024-11-21 19:57:52 +0300

    Made a change

--- farm_connect/urls.py
+++ farm_connect/urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('about/', TemplateView.as_view(template_name="about.html"), name='about'),
     path('farmer/dashboard/', TemplateView.as_view(template_name="farmer_dashboard.html"), name='farmer_dashboard'),
     path('supplier/dashboard/', TemplateView.as_view(template_name="supplier_dashboard.html"), name='supplier_dashboard')
