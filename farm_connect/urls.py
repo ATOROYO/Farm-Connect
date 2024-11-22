0c8750e1ef16623945d412645e42b9520c5849f1
ATOROYO <davidatoroyosika@gmail.com>
2024-11-22 23:24:06 +0300

    Made a change

--- farm_connect/urls.py
+++ farm_connect/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
+from apps.marketplace.views import about_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about/', TemplateView.as_view(template_name="about.html"), name='about'),
-    path('farmer/dashboard/', TemplateView.as_view(template_name="farmer_dashboard.html"), name='farmer_dashboard'),
-    path('supplier/dashboard/', TemplateView.as_view(template_name="supplier_dashboard.html"), name='supplier_dashboard')
+    path('about/', about_view, name='about'),
+    path('farmer/dashboard/', about_view, name='farmer_dashboard'),
+    path('supplier/dashboard/', about_view, name='supplier_dashboard')
 ]
