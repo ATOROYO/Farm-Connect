7261b1f7ab0435316b6d43c0dc768764e03323c1
ATOROYO <davidatoroyosika@gmail.com>
2024-11-23 22:54:34 +0300

    Created farmer_dashboard function in views.html

--- apps/marketplace/views.py
+++ apps/marketplace/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def about_view(request):
     return render(request, 'about.html')
+
+def farmer_dashboard(request):
+    return render(request, 'farmer_dashboard.html')
