aac4864eb382d0dde08999eef406af4bd5f7d256
ATOROYO <davidatoroyosika@gmail.com>
2024-11-23 22:58:19 +0300

    Imported the farmer_dashboard function

--- farm_connect/urls.py
+++ farm_connect/urls.py
@@ -6,6 +6,6 @@ from apps.marketplace.views import farmer_dashboard
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', about_view, name='about'),
-    path('farmer/dashboard/', about_view, name='farmer_dashboard'),
+    path('farmer/dashboard/', farmer_dashboard, name='farmer_dashboard'),
     path('supplier/dashboard/', about_view, name='supplier_dashboard')
 ]
