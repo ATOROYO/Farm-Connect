06d04fef42c38f22617a771fd117021a790a5d2b
ATOROYO <davidatoroyosika@gmail.com>
2024-11-23 23:04:43 +0300

    Updated the url

--- farm_connect/urls.py
+++ farm_connect/urls.py
@@ -6,6 +6,6 @@ from apps.marketplace.views import farmer_dashboard
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', about_view, name='about'),
-    path('farmer/dashboard/', farmer_dashboard, name='farmer_dashboard'),
+    path('marketplace/farmer/dashboard/', farmer_dashboard, name='farmer_dashboard'),
     path('supplier/dashboard/', about_view, name='supplier_dashboard')
 ]
