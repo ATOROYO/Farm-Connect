ee22383186ca1532268a86a28034150e8c7dee5f
ATOROYO <davidatoroyosika@gmail.com>
2024-11-27 00:21:44 +0300

    Added investments URLs in the project-wide urls.py

--- farm_connect/urls.py
+++ farm_connect/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, path
 from apps.marketplace.views import about_view
 from apps.marketplace.views import farmer_dashboard
 from apps.marketplace.views import supplier_dashboard
@@ -8,5 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', about_view, name='about'),
     path('marketplace/farmer/dashboard/', farmer_dashboard, name='farmer_dashboard'),
-    path('marketplace/supplier/dashboard/', supplier_dashboard, name='supplier_dashboard')
+    path('marketplace/supplier/dashboard/', supplier_dashboard, name='supplier_dashboard'),
+    path('investments/', include('apps.investments.urls')),
 ]
