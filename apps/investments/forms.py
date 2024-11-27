8ba69ab2c9c1b0b30d8bc8f9335984427e7161e0
ATOROYO <davidatoroyosika@gmail.com>
2024-11-27 18:25:34 +0300

    Updated the forms.py

--- apps/investments/forms.py
+++ apps/investments/forms.py
@@ -1,10 +1,24 @@
-from django import forms
-from .models import InvestmentProposal
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import FarmerProject, InvestmentProposal, InvestmentTracking
+from .forms import InvestmentProposalForm
 
-class ProposalForm(forms.ModelForm):
-    class Meta:
-        model = InvestmentProposal
-        fields = ['amount', 'message']
-        widgets = {
-            'message': forms.Textarea(attrs={'rows': 4}),
-        }
+@login_required
+def investors_view(request):
+    projects = FarmerProject.objects.all()
+    return render(request, 'investments/investors.html', {'projects': projects})
+
+@login_required
+def submit_proposal(request, project_id):
+    project = get_object_or_404(FarmerProject, id=project_id)
+    if request.method == 'POST':
+        form = InvestmentProposalForm(request.POST)
+        if form.is_valid():
+            proposal = form.save(commit=False)
+            proposal.project = project
+            proposal.investor = request.user
+            proposal.save()
+            return redirect('investors')
+    else:
+        form = InvestmentProposalForm()
+    return render(request, 'investments/submit_proposal.html', {'form': form, 'project': project})
