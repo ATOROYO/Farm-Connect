ef86f3b78b12dfc51fc9e963d28133a28a730653
Daniel Mule <165652173+danmule3@users.noreply.github.com>
2024-12-03 16:54:37 +0300

    Update views.py

--- apps/marketplace/views.py
+++ apps/marketplace/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 def about_view(request):
     return render(request, 'about.html')
 
+def contact_view(request):
+    return render(request, 'contact.html')
+
 def farmer_dashboard(request):
     return render(request, 'farmer_dashboard.html')
 
