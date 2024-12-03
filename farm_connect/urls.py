6da218d557bafcdc9626bac9a8c1b52731ea945f
Daniel Mule <165652173+danmule3@users.noreply.github.com>
2024-12-03 16:53:45 +0300

    Update urls.py

--- farm_connect/urls.py
+++ farm_connect/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
 from apps.marketplace.views import about_view
+from apps.marketplace.views import contact_view
 from apps.marketplace.views import farmer_dashboard
 from apps.marketplace.views import supplier_dashboard
 from apps.buyers.views import buyer_home
@@ -9,6 +10,7 @@ from apps.buyers.views import buyer_home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', about_view, name='about'),
+    path('contact/', contact_view, name='contact'),
     path('marketplace/farmer/dashboard/', farmer_dashboard, name='farmer_dashboard'),
     path('marketplace/supplier/dashboard/', supplier_dashboard, name='supplier_dashboard'),
     path('investments/', include('apps.investments.urls')),
