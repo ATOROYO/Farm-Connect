2b81fc58ed163658d8bd40ad3191b11007bbd1f4
ATOROYO <davidatoroyosika@gmail.com>
2024-12-04 18:02:24 +0300

    Updated the views.py

--- apps/investments/views.py
+++ apps/investments/views.py
@@ -1,27 +1,13 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import InvestmentOpportunity, FundingProposal
-from .forms import FundingProposalForm
+from django.shortcuts import render
 
+def browse_opportunities_view(request):
+    # Placeholder for browsing investment opportunities
+    return render(request, 'investments/browse_opportunities.html')
 
-def browse_opportunities(request):
-    opportunities = InvestmentOpportunity.objects.all()
-    return render(request, 'investments/browse.html', {'opportunities': opportunities})
+def submit_proposal_view(request):
+    # Placeholder for submitting funding proposals
+    return render(request, 'investments/submit_proposal.html')
 
-
-def submit_proposal(request, opportunity_id):
-    opportunity = get_object_or_404(InvestmentOpportunity, id=opportunity_id)
-    if request.method == 'POST':
-        form = FundingProposalForm(request.POST)
-        if form.is_valid():
-            proposal = form.save(commit=False)
-            proposal.investment_opportunity = opportunity
-            proposal.save()
-            return redirect('track_investments')
-    else:
-        form = FundingProposalForm()
-    return render(request, 'investments/submit_proposal.html', {'form': form, 'opportunity': opportunity})
-
-
-def track_investments(request):
-    proposals = FundingProposal.objects.all()
-    return render(request, 'investments/track.html', {'proposals': proposals})
+def track_investments_view(request):
+    # Placeholder for tracking investments
+    return render(request, 'investments/track_investments.html')
